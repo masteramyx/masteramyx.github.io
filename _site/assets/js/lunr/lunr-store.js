771a8dce1881a12d9da5e70bdb260e7870ef07e3
Kyle Amyx <kyle.amyx@duethealth.com>
2020-05-21 21:35:45 -0400

    .gitignore

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "2020-04-05-formula-one",
-        "excerpt":"R Markdown This is an R Markdown document. Markdown is a simple formatting syntax for authoring HTML, PDF, and MS Word documents. For more details on using R Markdown see http://rmarkdown.rstudio.com. When you click the Knit button a document will be generated that includes both content as well as the...","categories": [],
+        "excerpt":"F1 Predictive Modeling I can’t currently find the source of data but it is an extensive set made of multiple tables for years (1950-2017) x = c(rep(1,10), 1:5, 1:5, rep(1,10), 9:13, rep(11,10), 9:10) y &lt;- c(1:10, rep(10, 5), rep(7,5), 1:10, rep(1, 5), 1:10, 8:9) plot(x,y, pch=19, xlab=\"\", ylab = \"\",...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/formula-one/",
         "teaser":null},{
@@ -8,4 +8,9 @@ var store = [{
         "excerpt":"Anchors Value Added by Manufacturing Overview This research was a part of a greater project headed by Dr. William White and Dr. Richard Steckel of The Ohio State University’s Department of Economics. The core focus of the project was to look at the ways education was affected by early tractorization...","categories": [],
         "tags": ["EDA","regression"],
         "url": "http://localhost:4000/tractor-regression/",
+        "teaser":null},{
+        "title": "Tractor",
+        "excerpt":"Tractor/VAM Plots &amp; Models: Ohio Michigan Wisconsin Indiana Illinois Iowa VAM Regression Note about VAM Plots: Any counties with N/A value for VAM were replaced with 0. These are so few that I believe it has minimal effect on the trend lines. #Function for producing EDU dataset # # eduDataFunc...","categories": [],
+        "tags": ["EDA","regression"],
+        "url": "http://localhost:4000/tractor/",
         "teaser":null}]
