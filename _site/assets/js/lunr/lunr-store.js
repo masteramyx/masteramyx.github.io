13286229ae77afab29634d62275d3da89f75b698
Kyle Amyx <kyle.amyx@duethealth.com>
2020-06-22 10:08:10 -0400

    WIP: at code chunk PreProcessTweets

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -8,4 +8,9 @@ var store = [{
         "excerpt":"Overview This research was a part of a greater project headed by Dr. William White and Dr. Richard Steckel of The Ohio State University’s Department of Economics. The core focus of the project was to look at the ways education was affected by early tractorization and rural manufacturing. The Data...","categories": [],
         "tags": ["R"],
         "url": "http://localhost:4000/tractor/",
+        "teaser":null},{
+        "title": "Twitter Sentiment Analysis",
+        "excerpt":"Introduction Sentiment Analysis is the process of predicting whether a piece of information(usually text) indicates a positive, negative or neutral sentiment on a topic. This project allows the user to input a keyword and gathers tweets based on it, then analyzes the sentiments of those tweets. A classifier is Supervised...","categories": [],
+        "tags": ["Python"],
+        "url": "http://localhost:4000/twitter/",
         "teaser":null}]
